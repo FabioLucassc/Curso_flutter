8f527f29593530f753e98f3709c472c259f03dfa
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-10 10:55:35 -0300

    re-up

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+
+  runApp(new perguntaApp());
+}
+
+class perguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      home: Text('olá Flutter!!!')
+      
+    );
+  }
+} 
