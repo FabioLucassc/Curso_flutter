1459cb49f7738e881d85193efdf5e2b0db467cc8
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-11 08:41:06 -0300

    Iniciando

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
 main(List<String> args) {
-
-  runApp(new perguntaApp());
+  runApp(new PerguntaApp());
 }
 
-class perguntaApp extends StatelessWidget {
+class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual sua cor favorita?',
+      'Qual seu animal Favorito ?',
+    ];
 
     return MaterialApp(
-
-      home: Text('olá Flutter!!!')
-      
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(children: [
+          Text(perguntas[0]),
+          RaisedButton(
+            child: Text('Resposta1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Resposta2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Resposta3'),
+            onPressed: null,
+          ),
+        ]),
+      ),
     );
   }
-} 
+}
